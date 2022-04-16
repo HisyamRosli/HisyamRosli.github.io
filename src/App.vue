86f1655e2cd23df136bd1a6f099f112c06c9540b
HisyamRosli <82690198+HisyamRosli@users.noreply.github.com>
2022-04-16 15:27:56 +0800

    position fixing

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,13 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+   <img src="./assets/out.png" alt="" class="m-auto w-3/12"><br>
+    <p class="text-lg text-center">My Name is Hisyam Rosli, i am currently 24, 
+    i do freelancing PC build, fixing, troubleshooting. 
+    my hobbies, is that i practice freerunning/parkour, 
+    i also love to play card games like card fight vanguard. 
+    i am a Hardworking Telecommunication & IT graduate with proven leadership and management skills. Seeking to apply my abilities to the company where my academic can not only improve my status but also automate the working process to the best possible one. </p>
+
 </template>
 
 <style>
